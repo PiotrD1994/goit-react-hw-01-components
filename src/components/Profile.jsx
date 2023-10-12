fcf10db747936684799dab26fee19ef6959d2346
Piotr Dubiel <piotrdubiel30@gmail.com>
2023-10-12 22:28:41 +0200

    hw adding styles

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,33 +1,36 @@
 import React from "react";
 import PropTypes from 'prop-types'
+import css from './Profile.module.css'
 
-const Profile = ({username, tag, location, avatar, stats}) => {
-    return (
-        <div >
-        <div >
-          <img src={avatar} alt="User avatar"/>
-          <p >{username}</p>
-          <p >{tag}</p>
-          <p >{location}</p>
-        </div>
-  
-        <ul >
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt="User avatar" className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <div className={css.statsContainer}>
+        <ul className={css.stats}>
           <li>
-            <span >Followers</span>
-            <span >{stats.followers}</span>
+            <span className={css.label}>Followers</span>
+            <span className={css.quantity}>{stats.followers}</span>
           </li>
           <li>
-            <span >Views</span>
-            <span >{stats.views}</span>
+            <span className={css.label}>Views</span>
+            <span className={css.quantity}>{stats.views}</span>
           </li>
           <li>
-            <span >Likes</span>
-            <span >{stats.likes}</span>
+            <span className={css.label}>Likes</span>
+            <span className={css.quantity}>{stats.likes}</span>
           </li>
         </ul>
       </div>
-    )
-}
+    </div>
+  );
+};
 
 Profile.propTypes = {
     username: PropTypes.string.isRequired,
